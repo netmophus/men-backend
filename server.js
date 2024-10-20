f21b9820e19e429d014509d6f853a0f0d097581e
KANE ML <netmorphus@hotmail.com>
2024-10-20 01:22:01 +0100

    Fix CORS issue2

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://men-frontend.vercel.app/'],
+  origin: ['https://men-frontend.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // si vous utilisez des cookies ou des sessions
 }));
